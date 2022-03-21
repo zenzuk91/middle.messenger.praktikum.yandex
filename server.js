5f5e2b0da3d603ebe22f2cba79c0de3fb3f8b7b3
zenzuk <zenzuk91@ya.ru>
2022-03-21 18:03:28 +0300

    внёс правки по проекту

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ app.use(express.static('./dist'));
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
-<<<<<<< HEAD
+
 });
-=======
-});
->>>>>>> 567fbabb93eb8a3590c8f6bb2c9e618e5ce78b05
+
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+
+import html from 'bundle-text:./index.pug';
+
+document.body.innerHTML = html;
